19ed8a611d9eafda2ceeb468dd1b2557268bbdd2
Jack Graymer <jackgraymer@gmail.com>
2022-03-30 15:29:45 +0200

    Finished UI and backEnd, completelly working, missing animations

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let choice = ["rock", "paper", "scissor"];
 function computerPlay(){
     let random = choice[Math.floor(Math.random()*choice.length)]; 
-    console.log(random);
+    console.log('Computer choice : ' + random);
     return random;
 }
+
+
 
 /*function playerPlay(){
     var choice = window.prompt("Rock, Paper or Scissors?");
@@ -28,21 +30,36 @@ let computerCounter = 0;
 let tieCounter = 0;
 
 
-function game(C, P){
-    
+let C = '';
 
+function game(C, P){
+    C = computerChoice;
+    P = playerChoice;
+    W = ''
     if (C === P) {
-        alert( "It's a TIE between " + P + "s");
+        //alert( "It's a TIE between " + P + "s");
         tieCounter ++;
+        W = 'It was a TIE'
     } else if((C === "rock" && P === "scissor") || (C=== "paper" && P === "rock") || (C === "scissor" && P === "paper")) {
-        alert("You LOSE, " + C + " beats " + P);
+        //alert("You LOSE, " + C + " beats " + P);
         computerCounter ++;
+        document.getElementById('computerScore').innerText = computerCounter;
+        W = "Computer WINS";
         
     } else {
-        alert("You WIN, " + P + " beats " + C + " !");
+        //alert("You WIN, " + P + " beats " + C + " !");
         playerCounter ++;
+        document.getElementById('playerScore').innerText = playerCounter;
+        W = "Player WINS"
     } 
         console.log("Player " + playerCounter + " - " + "Computer " + computerCounter);
+
+    if (playerCounter == 5 || computerCounter == 5) {
+        alert("Want to play Again?")
+        location.reload();
+    }
+    
+    return W;
 }
 
 
@@ -60,11 +77,44 @@ if else
 
 */
 
-var rock = document.querySelectorAll('.card')
-rock.addEventListener("click", click);
+var playerChoice = document.querySelectorAll('.card');
+playerChoice.forEach(card => {card.addEventListener("click", click);})
 
 function click () {
-    console.log('Clicked ' + this.id);
+    //console.log('Clicked ' + this.id);
+    playerCard = this;
+    playerChoice = this.id;
+    computerChoice = computerPlay();
+    game();
+    Replacer();
+    round();
+    console.log('Player chose: ' + playerChoice);
     return this.id
+    
+}
+i = 1;
+function round(){
+    let history = document.querySelector('#history');
+    let newLine = document.createElement('p');
+    newLine.classList.add('newLine');
+    newLine.textContent = ' Round ' + i + ': Player chose ' + playerChoice + ", computer chose " + computerChoice + ' || ' + W;
+    i++;
+
+    history.prepend(newLine);
+    
 }
 
+function Replacer(){
+    function playerReplacer(){
+        //console.log(playerCard.children[1].firstElementChild)
+    document.querySelector('#player').children[1].firstElementChild.src = playerCard.children[1].firstElementChild.src;
+    }
+    playerReplacer();
+
+    function computerReplacer(){
+        document.querySelector('#computer').children[1].firstElementChild.src = computerChoice + '.png';
+        //console.log(document.querySelector('#computer').children[1].firstElementChild.src)
+    };
+    computerReplacer()
+    
+}
